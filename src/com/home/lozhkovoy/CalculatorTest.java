9cd31139e472fdad8583ad90fe1bac3134032642
IYJan <tbrn2009@gmail.com>
2020-07-28 09:39:22 +0400

    commit

--- src/com/home/lozhkovoy/CalculatorTest.java
+++ src/com/home/lozhkovoy/CalculatorTest.java
@@ -1,19 +1,19 @@
 package com.home.lozhkovoy;
 
-import java.util.Arrays;
+//import java.util.Arrays;
 import java.util.Scanner;
 
 public class CalculatorTest {
     private static char sign;
     private static int minNum = 1;
     private static int maxNum = 10;
-    private static int[] arab = {10 ,1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9};
-    private static String[] rome = {"X","I","II","III","IV","V","VI","VII","VIII","IX"};
+    private static int[] arab = {10, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    private static String[] rome = {"X", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+    private static String num1;
+    private static int romano;
+    private static int result;
 
     public static void main(String[] args) {
-        String num1;
-        int romano;
-        int result;
         Scanner reader = new Scanner(System.in);
         Calculator calc = new Calculator();
         RomanArabConvrt convrtRomArbConvrt = new RomanArabConvrt();
@@ -22,10 +22,10 @@ public class CalculatorTest {
         try {
             System.out.println("Введите арифметическую операцию:");
             num1 = reader.nextLine();
-            text = num1.replaceAll("\\s","");
-            String[] blocks = text.split("[+-/*]");
+            text = num1.replaceAll("\\s",""); // удалить все пробелы из введенной строки
+            String[] blocks = text.split("[+-/*]"); // поместить в строковый массив все переменные до знака и после любого из арифметических знаков
             romano = 0;
-            sign = checkSign(text);
+            sign = checkSign(text); // метод определения знака операции
 
             for (int i = 0; i <= 9; i++) {
                 if (rome[i].equals(blocks[0])) {
@@ -41,10 +41,10 @@ public class CalculatorTest {
                 System.out.println("Калькулятор умеет работать только с арабскими или римскими цифрами одновременно! ");
                 throw new IllegalArgumentException();
             }
-            if ((isNumsInRange(Integer.parseInt(blocks[0]))) && (isNumsInRange(Integer.parseInt(blocks[1])))) {
-                result = calc.calculate(Integer.parseInt(blocks[0]), Integer.parseInt(blocks[1]), sign);
+            if ((isNumsInRange(Integer.parseInt(blocks[0]))) && (isNumsInRange(Integer.parseInt(blocks[1])))) { //метод проверка входят ли введенные цифры в заданный диапазон 1...9
+                result = calc.calculate(Integer.parseInt(blocks[0]), Integer.parseInt(blocks[1]), sign); //выполнить арифметическую операцию в классе Calculator
             } else {
-                System.out.println("Введенные числа должны быть только >=1 и <=10");
+                System.out.println("Введенные числа должны быть только >=1 и <=10"); //выдать ошибку, если введенные числа вне диапазона 1...9
                 throw new IllegalArgumentException();
             }
             if (romano == 2) {
@@ -56,7 +56,8 @@ public class CalculatorTest {
             throw new IllegalArgumentException("Неверный формат данных");
         }
     }
-    private static char checkSign(String text) {
+
+    private static char checkSign(String text) { //определение знака арифметической операции
         if (text.indexOf("+") != -1) {
             return '+';
         } else if (text.indexOf("-") != -1) {
@@ -70,7 +71,8 @@ public class CalculatorTest {
             throw new IllegalArgumentException();
         }
     }
-    private static boolean isNumsInRange(int number) {
+
+    private static boolean isNumsInRange(int number) { //првоерка введенного числа >0 и <10
         return (number >= minNum && number <= maxNum);
     }
 }
